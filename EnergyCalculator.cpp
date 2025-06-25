332cbdd56b12dd919072e068f9eceba66176e6b6
fatinqistinah <fatinqistnah@gmail.com>
2025-06-25 19:31:02 +0800

    update the code

--- EnergyCalculator.cpp
+++ EnergyCalculator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 // Function to get AI energy-saving tip
@@ -14,16 +15,19 @@ string getAITip(double hoursPerDay, const string& applianceName) {
     }
 }
 int main() {
+    char proceed; // for continuity 
+    do{
     string applianceName; //declare variable
     double powerWatts = 0.0, hoursPerDay = 0.0;
 
     const double RATE_PER_KWH = 0.50;        // RM per kWh
     const double CO2_PER_KWH = 0.276;        // kg CO2 per kWh
-
+    
+    
     cout << "=== Smart Living Energy Calculator ===\n\n";
 
     cout << "Enter Appliance Name: "; //device name
-    getline(cin, applianceName);
+    getline (cin, applianceName);
 
     cout << "Enter Power Rating (Watts): "; //device power
     cin >> powerWatts;
@@ -37,7 +41,7 @@ int main() {
     double monthlyCO2 = dailyEnergyKWh * 30 * CO2_PER_KWH; //Monthly CO2 (kg)
     string tip = getAITip(hoursPerDay, applianceName);
 
-    // Output results 
+    // Output calculation results 
     cout << fixed << setprecision(2);
     cout << "\n--- Calculation Results ---\n";
     cout << "Appliance: " << applianceName << endl;
@@ -46,11 +50,26 @@ int main() {
     cout << "Estimated Monthly CO2 Emissions: " << monthlyCO2 << " kg" << endl;
 
     cout << "\nAI Energy Tip:\n" << tip << endl;
-// General energy tips
+    // General energy tips
     cout << "\n--- General Energy Saving Tips ---\n";
     cout << "- Peak Hours: Avoid using high-power appliances from 7-10 PM.\n";
     cout << "- Smart Scheduling: Use timers or smart plugs to control usage.\n";
     cout << "- Energy Efficiency: Choose ENERGY STAR certified appliances.\n";
+    
+        do {
+            cout << "Would you like to calculate for another appliance? (Y/N): ";
+            cin >> proceed;
+            // Check for any invalid input 
+            if (proceed != 'Y' && proceed != 'y' && proceed != 'N' && proceed != 'n') {
+                cout << "Invalid input. Please enter Y or N." << endl;
+            }
+            } while (proceed != 'Y' && proceed != 'y' && proceed != 'N' && proceed != 'n');
+            
+            if (proceed =='Y' || proceed == 'y'){
+                system("clear");// clear screen
+                cin.ignore();// clear input buffer
 
+            }
+    }while(proceed == 'Y' || proceed == 'y'); // loop if user enter Y or y
     return 0;
 }
